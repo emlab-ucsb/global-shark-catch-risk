b98661987cf0f0b313447c7c823ed5d70d9ef2d5
echelleburns <echelle_burns@ucsb.edu>
2022-03-31 17:21:31 -0700

    fix original model runs, play with using selector for models

---
+++ src/functions/func-all_rfmo_selector_models.R
@@ -0,0 +1,282 @@
+## Run All ML models - testing different efforts (reported with target species by flag, reported with bycatch by flag, reported by GFW) for each RFMO (without tuning) 
+
+#' Function that runs a wide variety of model scenarios for the WCPFC region
+#' @param data dataframe to use
+#' @param rfmos which rfmo to subset for
+#' #@param effort_source effort source option to use: "effort reported with target catch (flag)", "effort reported with bycatch (flag)", "gfw effort"
+#' @param save_loc output file loc to save r2
+
+all_rfmo_selector_models <- function(data, rfmos, effort_source, save_loc){
+  
+  # Fix data
+  data <- data %>% 
+    filter(rfmo == rfmos & 
+             gear_group == "longline") %>% 
+    mutate_if(is_character, as.factor) %>% 
+    arrange(year, latitude, longitude) %>% 
+    #fill(mean_sst:cv_chla, .direction = "downup") %>% 
+    #fill(sdm, .direction = "downup") %>% 
+    mutate(pres_abs = factor(ifelse(catch > 0, "present", "absent")),
+           zone = paste(latitude, longitude, sep = "|"))  
+  
+  if(effort_source == "effort reported with target catch (flag)") {
+    
+    # Subset by RFMO
+    prep_ll <- data %>% 
+      dplyr::select_if(!grepl("bycatch_total_effort", colnames(.)))
+    
+    # Remove columns where all target effort sums to 0
+    prep_ll <- prep_ll %>% 
+      dplyr::select(-colnames(prep_ll %>% # remove columns where target effort sums to 0
+                                dplyr::select_if(grepl("target_effort", colnames(.))) %>% 
+                                dplyr::select_if(colSums(., na.rm = T) == 0))) %>% 
+      distinct_all()
+  }
+  
+  if(effort_source == "effort reported with bycatch (flag)") {
+    
+    # Subset by RFMO
+    prep_ll <- data %>% 
+      dplyr::select_if(!grepl("target_effort", colnames(.)))
+    
+    # Remove columns where all bycatch associated effort sums to 0
+    prep_ll <- prep_ll %>% 
+      dplyr::select(-colnames(prep_ll %>% # remove columns where bycatch associated effort sums to 0
+                                dplyr::select_if(grepl("bycatch_total_effort", colnames(.))) %>% 
+                                dplyr::select_if(colSums(., na.rm = T) == 0))) %>% 
+      distinct_all()
+    
+    if(length(colnames(prep_ll)[grepl("bycatch_total_effort", colnames(prep_ll))]) == 0) { 
+      prep_ll <- data.frame()
+    }
+  }
+  if(effort_source == "gfw effort") { 
+    # Subset by RFMO
+    prep_ll <- data %>% 
+      filter(!is.na(total_fishing_kwh)) %>% 
+      distinct_all()
+  }
+  
+  if(nrow(prep_ll) > 0) { 
+    
+    # Add spatial groups
+    unique_pts <- prep_ll %>% 
+      dplyr::select(latitude, longitude) %>% 
+      distinct_all()
+    
+    # Convert to Spatial Points
+    xy <- SpatialPointsDataFrame(
+      matrix(c(unique_pts$longitude, unique_pts$latitude), ncol = 2), 
+      data.frame(ID = seq(1:nrow(unique_pts))), 
+      proj4string = CRS("+proj=longlat +ellps=WGS84 +datum=WGS84"))
+    
+    # Calculate distances between points
+    mdist <- distm(xy)
+    
+    # Complete heirarchical clustering based on dissimilarities
+    hc <- hclust(as.dist(mdist), method = "complete")
+    
+    # Use clustering to group locations into 5 or 24 groups
+    xy$clust <- cutree(hc, k = 5)
+    
+    unique_pts$location_cluster <- xy$clust
+    
+    # Add groups back to data
+    prep_ll <- prep_ll %>% 
+      left_join(unique_pts)
+    
+    # Clean column names 
+    prep_ll <- janitor::clean_names(prep_ll)
+    
+    ### 
+    # Run Models
+    ###
+    
+    split_data <- train_test_split(data = prep_ll, 
+                                   loc_group = "location_cluster", seed = 1234, prop = 0.25)
+    
+    # Assign training and testing data
+    train_data_class_orig <- split_data$training
+    
+    test_data_class_orig <- split_data$testing
+    
+    train_data_reg_orig <- split_data$training %>% 
+      filter(pres_abs == "present")
+    
+    test_data_reg_orig <- split_data$testing %>% 
+      filter(pres_abs == "present")
+    
+    # Only run the rest if there is data present
+    if(nrow(train_data_class_orig) > 0 & 
+       nrow(test_data_class_orig) > 0 & 
+       nrow(train_data_reg_orig) > 0 & 
+       nrow(test_data_reg_orig) > 0 ) {
+      
+      
+      train_class <- train_data_class_orig %>% 
+        select(-catch, -rfmo, -year, -spatial_notes, -gear_group, -species_group, -species_sciname, -catch_units,
+               -species_resolution,  -effort_units, -zone, -location_cluster)
+      
+      train_reg <- train_data_reg_orig %>% 
+        select(-pres_abs, -rfmo, -year, -spatial_notes, -gear_group, -species_group, -species_sciname, 
+               -catch_units, -species_resolution,  -effort_units, -zone, -location_cluster)
+      
+      final_test <- test_data_class_orig
+      
+      full_predict_df <- prep_ll
+      
+      # Start Recipes
+      class_recipe <- recipe(pres_abs ~ ., data = train_class) %>%
+        themis::step_upsample(pres_abs, over_ratio = 1) %>% 
+        step_center(all_numeric(), -all_outcomes(), - latitude, - longitude) %>% 
+        step_scale(all_numeric(), -all_outcomes(), - latitude, - longitude) %>%
+        step_unknown(all_nominal(), -all_outcomes()) %>%
+        step_dummy(all_nominal(), -all_outcomes()) %>% 
+        step_zv(all_predictors(), - latitude, - longitude) %>%
+        step_impute_knn(all_numeric(), -all_outcomes(), neighbors = 3, impute_with = imp_vars(latitude, longitude)) %>%
+        remove_role(latitude, longitude, old_role = "predictor") %>% 
+        recipeselectors::step_select_forests(all_predictors(), outcome = "pres_abs", top_p = 10) 
+      
+      reg_recipe <- recipe(catch ~ ., data = train_reg) %>%
+        step_center(all_numeric(), -all_outcomes(), - latitude, - longitude) %>% 
+        step_scale(all_numeric(), -all_outcomes(), - latitude, - longitude) %>%
+        step_unknown(all_nominal(), -all_outcomes()) %>%
+        step_dummy(all_nominal(), -all_outcomes()) %>% 
+        step_zv(all_predictors(), - latitude, - longitude) %>%
+        step_impute_knn(all_numeric(), -all_outcomes(), neighbors = 3, impute_with = imp_vars(latitude, longitude)) %>%
+        remove_role(latitude, longitude, old_role = "predictor") %>% 
+        recipeselectors::step_select_forests(all_predictors(), outcome = "catch", threshold = 0.9) 
+      
+      ###
+      # Classification Model
+      ###
+      ntrees = 500 
+      
+      class_model <- rand_forest(trees = ntrees) %>% 
+        set_engine("ranger", 
+                   importance = "impurity") %>% 
+        set_mode("classification") %>% 
+        translate()
+      
+      # Set Workflow
+      class_wflow <- 
+        workflow() %>% 
+        add_model(class_model) %>% 
+        add_recipe(class_recipe)
+      
+      # Fit Model
+      class_fit <- class_wflow %>% 
+        fit(data = train_class)
+      
+      prepped <- prep(class_recipe)
+      class_fit <- class_model  %>%
+        fit(pres_abs ~ ., juice(prepped))
+      recipeselectors::pull_importances(class_fit)
+      
+      # Return feature importance
+      importance_class <- class_fit %>% 
+        extract_fit_parsnip() %>% 
+        vip::vi()
+      
+      ###
+      # Regression Model
+      ###
+      reg_model <- rand_forest(trees = ntrees) %>% 
+        set_engine("ranger", 
+                   importance = "impurity") %>% 
+        set_mode("regression") %>% 
+        translate()
+      
+      # Set Workflow
+      reg_wflow <- 
+        workflow() %>% 
+        add_model(reg_model) %>% 
+        add_recipe(reg_recipe)
+      
+      # Fit Model
+      reg_fit <- reg_wflow %>% 
+        fit(data = train_reg)
+      
+      # Return feature importance
+      importance_reg <- reg_fit %>% 
+        extract_fit_parsnip() %>% 
+        vip::vi()
+      
+      ###
+      # Feature Importance
+      ###
+      total_importance <- importance_class %>% 
+        mutate(component = "classification") %>% 
+        bind_rows(importance_reg %>% 
+                    mutate(component = "regression"))
+      
+      ###
+      # Predict Testing Data
+      ###
+      final_class <- predict(class_fit, final_test) %>% 
+        mutate(.pred_class = ifelse(.pred_class == "absent", 0, 1)) %>% 
+        bind_cols(final_test)
+      
+      final_reg <- predict(reg_fit, final_test) %>% 
+        bind_cols(final_test)
+      
+      final_predict <- final_class %>% 
+        left_join(final_reg) %>% 
+        mutate(.final_pred = .pred_class*.pred) 
+      
+      final_metrics <- metrics(truth = catch, estimate = .final_pred, data = final_predict)
+      
+      final_metrics <- final_metrics %>% 
+        dplyr::select(-.estimator) %>% 
+        mutate(model = models, 
+               effort_source = effort_source, 
+               rfmo = rfmos) %>% 
+        pivot_wider(names_from = .metric, values_from = .estimate)
+      
+      ###
+      # Predict Full Dataset
+      ###
+      
+      full_predict_df <- full_predict_df %>% 
+        group_by(latitude, longitude) %>% 
+        mutate_at(vars(mean_sst:colnames(.)[ncol(.)]), mean, na.rm = T) %>% 
+        mutate_at(vars(mean_sst:colnames(.)[ncol(.)]), as.integer) %>% 
+        distinct_all()
+      
+      full_predict_class <- predict(class_fit, full_predict_df) %>% 
+        mutate(.pred_class = ifelse(.pred_class == "absent", 0, 1)) %>% 
+        bind_cols(full_predict_df)
+      
+      full_predict_reg <- predict(reg_fit, full_predict_df) %>% 
+        bind_cols(full_predict_df)
+      
+      full_predict <- full_predict_class %>% 
+        left_join(full_predict_reg) %>% 
+        mutate(.final_pred = .pred_class*.pred) %>% 
+        relocate(.pred, .before = .final_pred) %>% 
+        relocate(.pred_class, .before =.pred)
+      
+      full_metrics <- metrics(truth = catch, estimate = .final_pred, data = full_predict)
+      
+      full_metrics <- full_metrics %>% 
+        dplyr::select(-.estimator) %>% 
+        mutate(model = models, 
+               effort_source = effort_source, 
+               rfmo = rfmos) %>% 
+        pivot_wider(names_from = .metric, values_from = .estimate)
+    } 
+  }
+  
+  output_fit <- list("class_fit", class_fit, 
+                     "reg_fit" = reg_fit, 
+                     "class_predict" = final_class, 
+                     "reg_predict" = final_reg, 
+                     "final_predict" = final_predict, 
+                     "final_metrics" = final_metrics,
+                     "full_predict" = full_predict, 
+                     "full_metrcis" = full_metrics)
+  
+  saveRDS(output_fit, paste0(save_loc, rfmos, "_ll_final_model.rds"))
+  
+  write.csv(full_predict, paste0(save_loc, rfmos, "_ll_final_predict.csv"), row.names = FALSE)
+} 
