4ade9c0b909195cf62c37b03faafff060a258378
echelleburns <echelle_burns@ucsb.edu>
2022-06-06 11:36:41 -0700

    update setup for non-emlab collaborators

--- src/start_project.R
+++ src/start_project.R
@@ -10,3 +10,20 @@ project_path <- paste0(sys_path,"emlab/projects/current-projects/blue-prosperity
 project_data_path <- file.path(project_path,"broader-research/mpa-bycatch/data")
 # path to project figures
 project_figure_path <- file.path(project_path, "broader-research", "mpa-bycatch", "figures")
+
+
+# Edit working paths if it's a non-emLabber
+# setup should be: 
+# |- Google Drive
+#   |- My Drive
+#     |- mpa-bycatch
+#       |- data
+#       |- figures
+if(!dir.exists(sys_path)) { 
+  sys_path <- ifelse(Sys.info()["sysname"]=="Windows", "G:/My Drive/", # windows
+                     "~/Google Drive/My Drive/") # mac
+  emlab_data_path <- NULL
+  project_path <- paste0(sys_path, "mpa-bycatch")
+  project_data_path <- file.path(project_path, "data")
+  project_figure_path <- file.path(project_path, "figures")
+  }
