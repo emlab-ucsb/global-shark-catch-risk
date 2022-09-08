d4c28de52bdd3256abcc1034a9f1fe42ac6622d3
echelleburns <echelle_burns@ucsb.edu>
2022-09-07 17:06:42 -0700

    digging in a little more to tease out trends in overlap between catch, effort, and cpue

--- src/figures/figure_1.R
+++ src/figures/figure_1.R
@@ -139,31 +139,31 @@ legend <- get_legend(fig_1a)
 fig_1a <- fig_1a + 
   theme(legend.position = "none")
 
-# Figure 1b. cpue
+# Figure 1b. effort
 fig_1b <- ggplot() + 
-  geom_raster(mean_cpue_scaled, 
+  geom_raster(mean_bycatch_effort_scaled, 
               mapping = aes(x=x, y=y, fill=layer)) + 
   scale_fill_distiller("", 
-                       palette = "RdYlBu", na.value = NA, 
-                       breaks = c(min(mean_cpue_scaled$layer, na.rm = T), max(mean_cpue_scaled$layer, na.rm = T)), 
+                       palette = "RdYlBu", na.value = NA,
+                       breaks = c(min(mean_bycatch_effort_scaled$layer, na.rm = T), max(mean_bycatch_effort_scaled$layer, na.rm = T)), 
                        labels = c("Low", "High")) + 
   geom_sf(data = wcpfc_boundary, fill = NA, color = "black") + 
   geom_sf(data = iotc_boundary, fill = NA, color = "black") + 
   geom_sf(data = iccat_boundary, fill = NA, color = "black") + 
   geom_sf(data = iattc_boundary, fill = NA, color = "black") + 
   geom_tile(basemap_df %>% filter(!is.na(land_low_res_moll)), 
-              mapping = aes(x=x, y=y), fill = "black", color = "black") + 
+            mapping = aes(x=x, y=y), fill = "black", color = "black") + 
   coord_sf() + 
   custom_theme + 
   theme(legend.position = "none")
 
-# Figure 1c. effort
+# Figure 1c. cpue
 fig_1c <- ggplot() + 
-  geom_raster(mean_bycatch_effort_scaled, 
+  geom_raster(mean_cpue_scaled, 
               mapping = aes(x=x, y=y, fill=layer)) + 
   scale_fill_distiller("", 
-                       palette = "RdYlBu", na.value = NA,
-                       breaks = c(min(mean_bycatch_effort_scaled$layer, na.rm = T), max(mean_bycatch_effort_scaled$layer, na.rm = T)), 
+                       palette = "RdYlBu", na.value = NA, 
+                       breaks = c(min(mean_cpue_scaled$layer, na.rm = T), max(mean_cpue_scaled$layer, na.rm = T)), 
                        labels = c("Low", "High")) + 
   geom_sf(data = wcpfc_boundary, fill = NA, color = "black") + 
   geom_sf(data = iotc_boundary, fill = NA, color = "black") + 
@@ -187,3 +187,80 @@ final_plot <- ggdraw() +
 # Save
 ggsave(here::here("figures/final/figure_1.png"), final_plot,
        width = 10, height = 2.5, units = "in", dpi = 600, bg = "white")
+
+# Calculate how many cells overlap
+overlapping_cells <- mean_total_catch_scaled %>% 
+  filter(layer == max(layer, na.rm = T)) %>% 
+  mutate(name = "catch") %>% 
+  bind_rows(mean_bycatch_effort_scaled %>% 
+              filter(layer == max(layer, na.rm = T)) %>% 
+              mutate(name = "effort")) %>% 
+  bind_rows(mean_cpue_scaled %>% 
+              filter(layer == max(layer, na.rm = T)) %>% 
+              mutate(name = "cpue")) %>% 
+  select(-layer) %>% 
+  mutate(value = 1) %>% 
+  pivot_wider(names_from = name, values_from = value, values_fill = 0) %>% 
+  st_as_sf(., coords = c("x", "y"), crs = 4326)
+
+overlapping_proportions <- NULL
+
+for(layer in c("wcpfc_boundary", "iccat_boundary", "iotc_boundary", "iattc_boundary")) { 
+  
+  overlapping_proportions <- overlapping_proportions %>% 
+    bind_rows(st_intersection(overlapping_cells, eval(as.name(layer))) %>% 
+      as.data.frame() %>% 
+      select(-geometry) %>% 
+      mutate(combination = str_squish(paste0(ifelse(catch == 1, " catch ", ""), 
+                                             ifelse(effort == 1, " effort ", ""),
+                                             ifelse(cpue == 1, " cpue ", ""))), 
+             combination = case_when(combination %in% c("catch", "effort", "cpue") ~ "no overlap", 
+             TRUE ~ combination)) %>%
+      group_by(combination) %>% 
+      summarise(n = n()) %>% 
+      ungroup() %>% 
+      mutate(perc_overlap = n/sum(n)*100, 
+             rfmo = gsub("_boundary", "", layer)))
+}
+
+overlapping_proportions <- overlapping_proportions %>% 
+  select(-n) %>% 
+  pivot_wider(names_from = combination, values_from = perc_overlap)
+
+write.csv(overlapping_proportions, 
+          here::here("tables/supplemental/overlapping_catch_effort_cpue.csv"), 
+          row.names = FALSE)
+
+# Figure of areas with high CPUE
+ggplot() + 
+  geom_tile(mean_cpue_scaled %>% 
+            filter(layer == max(layer, na.rm = T)), 
+            mapping = aes(x=x, y=y), fill = "blue") + 
+  geom_sf(data = wcpfc_boundary, fill = NA, color = "black") + 
+  geom_sf(data = iotc_boundary, fill = NA, color = "black") + 
+  geom_sf(data = iccat_boundary, fill = NA, color = "black") + 
+  geom_sf(data = iattc_boundary, fill = NA, color = "black") + 
+  geom_tile(basemap_df %>% filter(!is.na(land_low_res_moll)), 
+            mapping = aes(x=x, y=y), fill = "black", color = "black") + 
+  coord_sf() + 
+  custom_theme + 
+  theme(legend.position = "none")
+
+# Grab species catches within those areas
+rfmo_spp_totals <- all_dat %>% 
+  group_by(latitude, longitude, year, rfmo, species_commonname) %>% 
+  summarise(total_catch = sum(catch, na.rm = T), 
+            total_bycatch_effort = mean(bycatch_total_effort, na.rm = T), # repeated across individuals
+            total_cpue = total_catch/total_bycatch_effort) %>% 
+  ungroup() %>%
+  group_by(latitude, longitude, rfmo, species_commonname) %>% 
+  summarise(mean_total_catch = mean(total_catch, na.rm = T), 
+            mean_bycatch_effort = mean(total_bycatch_effort, na.rm = T), 
+            mean_cpue = mean(total_cpue, na.rm = T))%>% 
+  ungroup() %>% 
+  mutate(latitude_temp = latitude, 
+         longitude_temp = longitude) %>% 
+  st_as_sf(., coords = c("longitude_temp", "latitude_temp"), crs = 4326) 
+
+rfmo_spp_totals_overlap <- st_intersection(rfmo_spp_totals, 
+                                           overlapping_cells)
