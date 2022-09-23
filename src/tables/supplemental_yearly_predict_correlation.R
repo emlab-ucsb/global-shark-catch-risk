4d081ad28c5dbe8e2977077f774c6bb3d322f00a
echelleburns <echelle_burns@ucsb.edu>
2022-09-23 12:09:43 -0700

    save outputs and make figure for yearly hotspots

--- src/tables/supplemental_yearly_predict_correlation.R
+++ src/tables/supplemental_yearly_predict_correlation.R
@@ -12,6 +12,9 @@ library(tmap)
 library(here)
 library(ape)
 
+# Load plotting defaults
+source(file.path(here::here(), "src/figures/plot_defaults.R"))
+
 # Load data - use cleaned data at the 1x1 resolution using count (not mt converted to count)
 list_files <- list.files(file.path(here::here(), "data-updated/model-data/outputs/all-rfmo-models"), 
                          pattern = "_untuned_final_predict", full.names = TRUE)
@@ -70,6 +73,12 @@ for(file in list_files) {
   
   remove(temp)
 }
+
+all_dat <- all_dat %>% mutate(cell_id = paste(longitude, latitude, rfmo, sep = "|"))
+
+# Save output
+write.csv(all_dat, file.path(here::here(), "data-updated", "model-data", "outputs", "predicted_catch_yearly.csv"), row.names = F)
+
 
 # # Calculate Moran's I for each year - using this example (https://stats.oarc.ucla.edu/r/faq/how-can-i-calculate-morans-i-in-r/)
 # 
@@ -236,3 +245,151 @@ ggplot() +
 
 ggsave(file.path(here::here(), "figures", "supplemental", "ar_results.png"), bg = "white")
 
+
+## Clustering plot for high vs low vs intermediate cells
+# Create yearly thresholds 
+# Calculate cells that are high risk (0.9), low risk (0.1), and intermediate
+summarized_dat_yr <- all_dat %>% 
+  group_by(latitude, longitude, year) %>% 
+  summarise(.final_pred = sum(.final_pred, na.rm = T)) %>% 
+  ungroup() 
+
+thresholds <- NULL
+for(yr in unique(summarized_dat_yr$year)) { 
+  temp <- summarized_dat_yr %>% filter(year == yr) %>% filter(.final_pred > 0)
+  
+  thresholds <- thresholds %>% 
+    bind_rows(data.frame("year" = yr, 
+                         "low_threshold" = as.numeric(quantile(temp$.final_pred, 0.1)), 
+                         "high_threshold" = as.numeric(quantile(temp$.final_pred, 0.9))))
+}
+
+summarized_dat_yr <- summarized_dat_yr %>% 
+  left_join(thresholds) %>% 
+  group_by(year, latitude, longitude) %>% 
+  mutate(risk_level = case_when(.final_pred >= high_threshold ~ "high", 
+                                .final_pred <= low_threshold ~ "low", 
+                                TRUE ~ "intermediate")) %>% 
+  ungroup() %>% 
+  select(latitude, longitude, year, risk_level)
+
+all_dat2 <- all_dat %>% 
+  group_by(rfmo, year, latitude, longitude) %>% 
+  summarise(.final_pred = sum(.final_pred, na.rm = T), 
+            catch = sum(catch, na.rm = T)) %>% 
+  ungroup() %>% 
+  left_join(summarized_dat_yr)
+
+for(yr in unique(all_dat2$year)) { 
+  
+  dat_temp <- all_dat2 %>% 
+      filter(year == yr)
+  
+  dat_temp <- dat_temp %>%
+    mutate(risk_level = case_when(risk_level == "low" ~ 1, 
+                                  risk_level == "intermediate" ~ 2, 
+                                  risk_level == "high" ~ 3), 
+           longitude_orig = longitude, 
+           latitude_orig = latitude) %>% 
+    st_as_sf(., coords = c("longitude_orig", "latitude_orig"), crs = 4326)
+  
+  raster_stack <- stack()
+  
+  raster_1 <- dat_temp %>% 
+    filter(latitude%%1 == 0 & longitude%%1 == 0) 
+  
+  if(nrow(raster_1) > 0) { 
+    raster_1 <- raster_1 %>% 
+      rasterize(., whole_numbers, field = "risk_level", fun = max, background = NA)
+    
+    raster_stack <- stack(raster_stack, raster_1)
+  }
+  
+  raster_2 <- dat_temp %>% 
+    filter(latitude%%1 != 0 & longitude%%1 == 0) 
+  
+  if(nrow(raster_2) > 0) { 
+    raster_2 <- raster_2 %>% 
+      rasterize(., whole_numbers, field = "risk_level", fun = max, background = NA)
+    
+    raster_stack <- stack(raster_stack, raster_2)
+  }
+  
+  raster_3 <- dat_temp %>% 
+    filter(latitude%%1 == 0 & longitude%%1 != 0)  
+  
+  if(nrow(raster_3) > 0) { 
+    raster_3 <- raster_3 %>% 
+      rasterize(., whole_numbers, field = "risk_level", fun = max, background = NA)
+    
+    raster_stack <- stack(raster_stack, raster_3)
+  }
+  
+  raster_4 <- dat_temp %>% 
+    filter(latitude%%1 != 0 & longitude%%1 != 0)  
+  
+  if(nrow(raster_4) > 0) { 
+    raster_4 <- raster_4 %>% 
+      rasterize(., whole_numbers, field = "risk_level", fun = max, background = NA)
+    
+    raster_stack <- stack(raster_stack, raster_4)
+  }
+  
+  raster_comb <- calc(raster_stack, max, na.rm = T)
+  
+  raster_comb <- raster::as.data.frame(raster_comb, xy = TRUE) %>% 
+    mutate(layer = as.factor(layer))
+  
+  plot_t <- ggplot() + 
+    geom_tile(raster_comb, 
+              mapping = aes(x=x, y=y, fill=layer, color = layer)) + 
+    scale_fill_manual("Risk Level", values = c("khaki", "orange", "darkred"), na.value = NA, 
+                         breaks = c(1,2,3), 
+                         labels = c("Low", "Intermediate", "High"), 
+                         guide = guide_legend(title.vjust = 0.8)) + 
+    scale_color_manual("", values = c("khaki", "orange", "darkred"), na.value = NA, 
+                          breaks = c(1,2,3), guide = "none") + 
+    geom_sf(data = wcpfc_boundary, fill = NA, color = "black") +
+    geom_sf(data = iotc_boundary, fill = NA, color = "black") +
+    geom_sf(data = iccat_boundary, fill = NA, color = "black") +
+    geom_sf(data = iattc_boundary, fill = NA, color = "black") +
+    geom_tile(basemap_df %>% filter(!is.na(land_low_res_moll)),
+              mapping = aes(x=x, y=y), fill = "black", color = "black") +
+    coord_sf() + 
+    custom_theme + 
+    theme(legend.position = "bottom", 
+          text = element_text(size = 18)) 
+  
+  if(yr == 2012) { 
+    legend_year <- get_legend(plot_t)
+  }
+  
+  plot_t <- plot_t + 
+    theme(legend.position = "none")
+  
+  assign(paste0("yearly_", yr, "_plot"), plot_t)
+  
+  # assign(paste0("yearly_", yr), raster_comb)
+} 
+
+# 3x3 plot
+
+final_plot <- 
+  ggdraw() + 
+  draw_plot(yearly_2012_plot, 0, 0.7, 0.33, 0.3) + 
+  draw_plot(yearly_2013_plot, 0.33, 0.7, 0.33, 0.3) + 
+  draw_plot(yearly_2014_plot, 0.66, 0.7, 0.33, 0.3) + 
+  draw_plot(yearly_2015_plot, 0, 0.4, 0.33, 0.3) + 
+  draw_plot(yearly_2016_plot, 0.33, 0.4, 0.33, 0.3) + 
+  draw_plot(yearly_2017_plot, 0.66, 0.4, 0.33, 0.3) + 
+  draw_plot(yearly_2018_plot, 0, 0.1, 0.33, 0.32) + 
+  draw_plot(yearly_2019_plot, 0.33, 0.1, 0.33, 0.3) + 
+  draw_plot(yearly_2020_plot, 0.66, 0.1, 0.33, 0.3) + 
+  draw_plot(legend_year, 0, 0, 1, 0.1) + 
+  draw_plot_label(label = paste0(LETTERS[1:9], ") ", 2012:2020), x = rep(c(0, 0.33, 0.66), 3), 
+                  y = rep(c(1, 0.7, 0.4), each = 3))
+
+# Save
+ggsave(file.path(here::here(), "figures", "supplemental", "yearly_hotspots.png"), 
+       final_plot, bg = "white", dpi = 600, width = 20, height = 12)
+  
