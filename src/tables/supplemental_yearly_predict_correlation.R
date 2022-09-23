da2e6528180e907785485683afb1e3b50e2e9af1
echelleburns <echelle_burns@ucsb.edu>
2022-09-23 12:16:06 -0700

    save csvs and comment out unused code

--- src/tables/supplemental_yearly_predict_correlation.R
+++ src/tables/supplemental_yearly_predict_correlation.R
@@ -148,102 +148,102 @@ write.csv(all_dat, file.path(here::here(), "data-updated", "model-data", "output
 
 # Run autoregressive (AR) model
 # An AR is an arima(1,0,0) model (https://financetrain.com/estimating-autoregressive-ar-model-in-r)
-all_dat_pivot <- all_dat %>% 
-  mutate(cell_id = paste(longitude, latitude, rfmo, sep = "|")) %>% 
-  group_by(year, latitude, longitude, rfmo, cell_id) %>% 
-  summarise(.final_pred = sum(.final_pred, na.rm = T)) %>% 
-  ungroup() %>% 
-  pivot_wider(values_from = .final_pred, names_from = year) %>% 
-  pivot_longer(-c(cell_id, latitude, longitude, rfmo), names_to = "year", values_to = ".final_pred") %>% 
-  arrange(year, longitude, latitude)
-
-ar_results <- NULL 
-for(cell in unique(all_dat_pivot$cell_id)) { 
-  temp <- all_dat_pivot %>% filter(cell_id == cell)
-  
-  min_year <- min(temp$year[which(!is.na(temp$.final_pred))])
-  max_year <- max(temp$year[which(!is.na(temp$.final_pred))])
-  
-  temp <- temp %>% 
-    filter(year >= min_year & year <= max_year)
-  
-  if(nrow(temp) < 2) {next}
-  
-  temp_test <- temp %>% filter(!is.na(.final_pred))
-  
-  if(temp_test$year[1] == min_year & temp_test$year[2] == max_year) { next }
-  
-  if(length(unique(temp$.final_pred[!is.na(temp$.final_pred)])) == 1) { next }
-  
-  ts_temp <- ts(temp$.final_pred, start = min_year, end = max_year, frequency = 1)
-  
-  arima_temp <- arima(ts_temp[!is.na(ts_temp)], order = c(1,0,0), method = "ML")
-  
-  ar_results <- bind_rows(ar_results, 
-                          data.frame("cell_id" = cell, 
-                                     "year_start" = min_year, 
-                                     "year_end" = max_year,
-                                     "n_nas" = length(ts_temp[is.na(ts_temp)]),
-                                     "ar1" = arima_temp$coef[1], 
-                                     "intercept" = arima_temp$coef[2], 
-                                     "sigma2" = arima_temp$sigma2, 
-                                     "log_likelihood" = arima_temp$loglik, 
-                                     "aic" = arima_temp$aic))
-  }
-
-write.csv(ar_results,  file.path(here::here(), "tables", "supplemental", "ar_results.csv"), 
-                    row.names = F)
-
-ar_results <- read.csv(file.path(here::here(), "tables", "supplemental", "ar_results.csv"))
-
-ar_results <- ar_results %>% 
-  separate(cell_id, into = c("longitude", "latitude", "rfmo"), sep = "[|]", remove = FALSE)
-
-ggplot() + 
-  geom_boxplot(data = ar_results, mapping = aes(x = rfmo, y = ar1))
-
-# Calculate cells that are high risk (0.9), low risk (0.1), and intermediate
-summarized_dat <- all_dat %>% 
-  group_by(latitude, longitude, rfmo) %>% 
-  summarise(.final_pred = sum(.final_pred, na.rm = T)) %>% 
-  ungroup() 
-
-thresholds <- NULL
-for(rfmos in unique(summarized_dat$rfmo)) { 
-  temp <- summarized_dat %>% filter(rfmo == rfmos) %>% filter(.final_pred > 0)
-  
-  thresholds <- thresholds %>% 
-    bind_rows(data.frame("rfmo" = rfmos, 
-                         "low_threshold" = as.numeric(quantile(temp$.final_pred, 0.1)), 
-                         "high_threshold" = as.numeric(quantile(temp$.final_pred, 0.9))))
-}
-
-summarized_dat <- summarized_dat %>% 
-  left_join(thresholds) %>% 
-  group_by(rfmo, latitude, longitude) %>% 
-  mutate(risk_level = case_when(.final_pred >= high_threshold ~ "high", 
-                                .final_pred <= low_threshold ~ "low", 
-                                TRUE ~ "intermediate")) %>% 
-  ungroup() %>% 
-  select(latitude, longitude, rfmo, risk_level)
-
-ar_results <- ar_results %>% 
-  mutate(latitude = as.numeric(latitude), 
-         longitude = as.numeric(longitude)) %>% 
-  left_join(summarized_dat)
-
-
-ggplot() + 
-  geom_boxplot(data = ar_results, mapping = aes(x = rfmo, y = ar1, fill = risk_level), 
-               alpha = 0.5) + 
-  xlab("") + 
-  ylab("AR slope (AR1)") + 
-  scale_fill_manual("Risk Level", values = c("high" = "firebrick1", 
-                                             "intermediate" = "gold", 
-                                             "low" = "olivedrab")) + 
-  theme_classic()
-
-ggsave(file.path(here::here(), "figures", "supplemental", "ar_results.png"), bg = "white")
+# all_dat_pivot <- all_dat %>% 
+#   mutate(cell_id = paste(longitude, latitude, rfmo, sep = "|")) %>% 
+#   group_by(year, latitude, longitude, rfmo, cell_id) %>% 
+#   summarise(.final_pred = sum(.final_pred, na.rm = T)) %>% 
+#   ungroup() %>% 
+#   pivot_wider(values_from = .final_pred, names_from = year) %>% 
+#   pivot_longer(-c(cell_id, latitude, longitude, rfmo), names_to = "year", values_to = ".final_pred") %>% 
+#   arrange(year, longitude, latitude)
+# 
+# ar_results <- NULL 
+# for(cell in unique(all_dat_pivot$cell_id)) { 
+#   temp <- all_dat_pivot %>% filter(cell_id == cell)
+#   
+#   min_year <- min(temp$year[which(!is.na(temp$.final_pred))])
+#   max_year <- max(temp$year[which(!is.na(temp$.final_pred))])
+#   
+#   temp <- temp %>% 
+#     filter(year >= min_year & year <= max_year)
+#   
+#   if(nrow(temp) < 2) {next}
+#   
+#   temp_test <- temp %>% filter(!is.na(.final_pred))
+#   
+#   if(temp_test$year[1] == min_year & temp_test$year[2] == max_year) { next }
+#   
+#   if(length(unique(temp$.final_pred[!is.na(temp$.final_pred)])) == 1) { next }
+#   
+#   ts_temp <- ts(temp$.final_pred, start = min_year, end = max_year, frequency = 1)
+#   
+#   arima_temp <- arima(ts_temp[!is.na(ts_temp)], order = c(1,0,0), method = "ML")
+#   
+#   ar_results <- bind_rows(ar_results, 
+#                           data.frame("cell_id" = cell, 
+#                                      "year_start" = min_year, 
+#                                      "year_end" = max_year,
+#                                      "n_nas" = length(ts_temp[is.na(ts_temp)]),
+#                                      "ar1" = arima_temp$coef[1], 
+#                                      "intercept" = arima_temp$coef[2], 
+#                                      "sigma2" = arima_temp$sigma2, 
+#                                      "log_likelihood" = arima_temp$loglik, 
+#                                      "aic" = arima_temp$aic))
+#   }
+# 
+# write.csv(ar_results,  file.path(here::here(), "tables", "supplemental", "ar_results.csv"), 
+#                     row.names = F)
+# 
+# ar_results <- read.csv(file.path(here::here(), "tables", "supplemental", "ar_results.csv"))
+# 
+# ar_results <- ar_results %>% 
+#   separate(cell_id, into = c("longitude", "latitude", "rfmo"), sep = "[|]", remove = FALSE)
+# 
+# ggplot() + 
+#   geom_boxplot(data = ar_results, mapping = aes(x = rfmo, y = ar1))
+# 
+# # Calculate cells that are high risk (0.9), low risk (0.1), and intermediate
+# summarized_dat <- all_dat %>% 
+#   group_by(latitude, longitude, rfmo) %>% 
+#   summarise(.final_pred = sum(.final_pred, na.rm = T)) %>% 
+#   ungroup() 
+# 
+# thresholds <- NULL
+# for(rfmos in unique(summarized_dat$rfmo)) { 
+#   temp <- summarized_dat %>% filter(rfmo == rfmos) %>% filter(.final_pred > 0)
+#   
+#   thresholds <- thresholds %>% 
+#     bind_rows(data.frame("rfmo" = rfmos, 
+#                          "low_threshold" = as.numeric(quantile(temp$.final_pred, 0.1)), 
+#                          "high_threshold" = as.numeric(quantile(temp$.final_pred, 0.9))))
+# }
+# 
+# summarized_dat <- summarized_dat %>% 
+#   left_join(thresholds) %>% 
+#   group_by(rfmo, latitude, longitude) %>% 
+#   mutate(risk_level = case_when(.final_pred >= high_threshold ~ "high", 
+#                                 .final_pred <= low_threshold ~ "low", 
+#                                 TRUE ~ "intermediate")) %>% 
+#   ungroup() %>% 
+#   select(latitude, longitude, rfmo, risk_level)
+# 
+# ar_results <- ar_results %>% 
+#   mutate(latitude = as.numeric(latitude), 
+#          longitude = as.numeric(longitude)) %>% 
+#   left_join(summarized_dat)
+# 
+# 
+# ggplot() + 
+#   geom_boxplot(data = ar_results, mapping = aes(x = rfmo, y = ar1, fill = risk_level), 
+#                alpha = 0.5) + 
+#   xlab("") + 
+#   ylab("AR slope (AR1)") + 
+#   scale_fill_manual("Risk Level", values = c("high" = "firebrick1", 
+#                                              "intermediate" = "gold", 
+#                                              "low" = "olivedrab")) + 
+#   theme_classic()
+# 
+# ggsave(file.path(here::here(), "figures", "supplemental", "ar_results.png"), bg = "white")
 
 
 ## Clustering plot for high vs low vs intermediate cells
@@ -264,6 +264,9 @@ for(yr in unique(summarized_dat_yr$year)) {
                          "high_threshold" = as.numeric(quantile(temp$.final_pred, 0.9))))
 }
 
+# Save output
+write.csv(thresholds, file.path(here::here(), "data-updated", "model-data", "outputs", "yearly_risk_thresholds.csv"), row.names = F)
+
 summarized_dat_yr <- summarized_dat_yr %>% 
   left_join(thresholds) %>% 
   group_by(year, latitude, longitude) %>% 
@@ -274,11 +277,14 @@ summarized_dat_yr <- summarized_dat_yr %>%
   select(latitude, longitude, year, risk_level)
 
 all_dat2 <- all_dat %>% 
-  group_by(rfmo, year, latitude, longitude) %>% 
+  group_by(rfmo, year, latitude, longitude, cell_id) %>% 
   summarise(.final_pred = sum(.final_pred, na.rm = T), 
             catch = sum(catch, na.rm = T)) %>% 
   ungroup() %>% 
   left_join(summarized_dat_yr)
+
+# Save output
+write.csv(all_dat2, file.path(here::here(), "data-updated", "model-data", "outputs", "predicted_catch_yearly_risk.csv"), row.names = F)
 
 for(yr in unique(all_dat2$year)) { 
   
