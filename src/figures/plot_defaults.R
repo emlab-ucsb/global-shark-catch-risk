5ad330b2df1e111f381bda99a7748595916da5ad
Echelle Burns <echelle.burns@gmail.com>
2022-10-04 16:45:12 -0700

    fix data path

--- src/figures/plot_defaults.R
+++ src/figures/plot_defaults.R
@@ -1,12 +1,12 @@
 # Plotting defaults for figures
 
 # Basemap
-basemap_raster <- raster::raster(file.path(here::here(), "data/mapping-templates/land_low_res_moll.tif")) %>% 
+basemap_raster <- raster::raster(file.path(here::here(), "data-updated/mapping-templates/land_low_res_moll.tif")) %>% 
   projectRaster(., crs = 4326)
 basemap_df <- raster::as.data.frame(basemap_raster, xy = T) %>% 
   mutate(land_low_res_moll = ifelse(land_low_res_moll >= 0.9, 1, NA))
 
-# Four options for rasterizing RFMO data (depends on RFMO location reporting)
+# Four options for rasterizing RFMO data-updated (depends on RFMO location reporting)
 whole_numbers <- raster::raster(xmn = -179, xmx = 179, ymn = -89, ymx = 89, crs = 4326, resolution = 1) #%>% 
   #projectRaster(., crs = basemap_raster)
 whole_numbers_lon <- raster::raster(xmn = -179, xmx = 179, ymn = -89.5, ymx = 89.5, crs = 4326, resolution = 1) #%>% 
@@ -17,10 +17,10 @@ fraction_numbers <- raster::raster(xmn = -179.5, xmx = 179.5, ymn = -89.5, ymx =
   #projectRaster(., crs = basemap_raster)
 
 # RFMO boundaries
-wcpfc_boundary <- sf::st_read(file.path(here::here(), "data/rfmo-boundaries/RFB_WCPFC/RFB_WCPFC.shp"))
-iotc_boundary <- sf::st_read(file.path(here::here(), "data/rfmo-boundaries/RFB_IOTC/RFB_IOTC.shp"))
-iccat_boundary <- sf::st_read(file.path(here::here(), "data/rfmo-boundaries/RFB_ICCAT/RFB_ICCAT.shp"))
-iattc_boundary <- sf::st_read(file.path(here::here(), "data/rfmo-boundaries/RFB_IATTC/RFB_IATTC.shp"))
+wcpfc_boundary <- sf::st_read(file.path(here::here(), "data-updated/rfmo-boundaries/RFB_WCPFC/RFB_WCPFC.shp"))
+iotc_boundary <- sf::st_read(file.path(here::here(), "data-updated/rfmo-boundaries/RFB_IOTC/RFB_IOTC.shp"))
+iccat_boundary <- sf::st_read(file.path(here::here(), "data-updated/rfmo-boundaries/RFB_ICCAT/RFB_ICCAT.shp"))
+iattc_boundary <- sf::st_read(file.path(here::here(), "data-updated/rfmo-boundaries/RFB_IATTC/RFB_IATTC.shp"))
 
 # map theme
 custom_theme <- theme_void() 
